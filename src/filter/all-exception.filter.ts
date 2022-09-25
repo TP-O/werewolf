ca6e7f4dcdd76c99b20a826e42ca109ac329272d
TP-O <letranphong2k1@gmail.com>
2022-09-25 18:42:55 +0700

    feat: log unexpected exceptions

--- src/filter/all-exception.filter.ts
+++ src/filter/all-exception.filter.ts
@@ -1,13 +1,18 @@
 import { ExceptionFilter, Catch, ArgumentsHost } from '@nestjs/common';
-import { FastifyReply } from 'fastify';
+import { FastifyReply, FastifyRequest } from 'fastify';
 import { Socket } from 'socket.io';
 import { AppConfig } from 'src/config';
+import { Log } from 'src/decorator/log.decorator';
 import { EmitEvent } from 'src/enum';
-import { EmitEvents } from 'src/type';
+import { EmitEvents, LoggedError } from 'src/type';
+import { Logger } from 'winston';
 
 @Catch()
 export class AllExceptionFilter implements ExceptionFilter {
-  catch(exception: Error, host: ArgumentsHost) {
+  @Log()
+  private logger: Logger;
+
+  catch(exception: LoggedError, host: ArgumentsHost) {
     if (AppConfig.debug) {
       throw exception;
     }
@@ -24,10 +29,16 @@ export class AllExceptionFilter implements ExceptionFilter {
       case 'rpc':
         break;
     }
+
+    this.logger.error(exception);
   }
 
-  private handleWsException(exception: Error, host: ArgumentsHost) {
+  private handleWsException(exception: LoggedError, host: ArgumentsHost) {
     const client = host.switchToWs().getClient() as Socket<null, EmitEvents>;
+
+    exception.hostType = 'ws';
+    exception.event = client.eventName;
+    exception.payload = host.switchToWs().getData();
 
     client.emit(EmitEvent.Error, {
       event: client.eventName,
@@ -35,8 +46,13 @@ export class AllExceptionFilter implements ExceptionFilter {
     });
   }
 
-  private handleHttpException(exception: Error, host: ArgumentsHost) {
+  private handleHttpException(exception: LoggedError, host: ArgumentsHost) {
     const response = host.switchToHttp().getResponse<FastifyReply>();
+    const request = host.switchToHttp().getRequest<FastifyRequest>();
+
+    exception.hostType = 'http';
+    exception.url = request.url;
+    exception.payload = request.body;
 
     response.code(500).send({
       statusCode: 500,
--- src/type/error.type.ts
+++ src/type/error.type.ts
@@ -11,3 +11,10 @@ export type WsErrorResponse = {
   event: ListenEvent | null;
   message: ErrorMessage;
 };
+
+export type LoggedError = Error & {
+  hostType: string;
+  event?: ListenEvent | null;
+  url?: string;
+  payload: any;
+};
