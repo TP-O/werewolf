0bb5b2f07a5c65c436abd309a5ac040acf16f953
Phong Le <69zemo@gmail.com>
2023-02-26 22:26:55 +0700

    feat: change phase day dusk night

---
+++ client/Werewolf/Assets/Assets/DayNightSystem2D/Scripts/DayNightSystem2D.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DayCycles
+{
+    Night = 0,
+    Day = 1,
+    Dusk = 2
+}
+
+public class DayNightSystem2D : MonoBehaviour
+{
+    [Header("Controllers")]
+    public UnityEngine.Rendering.Universal.Light2D globalLight;
+    //public float cycleCurrentTime = 0;
+    public float cycleMaxTime = 5;
+    public float colorChangeDuration = 2f;
+    public DayCycles dayCycle = DayCycles.Night;
+
+    [Header("Cycle Colors")]
+    public Color night;
+    public Color day;
+    public Color dusk;
+
+    [Header("Objects")]
+    public UnityEngine.Rendering.Universal.Light2D[] mapLights;
+
+    void Start()
+    {
+        dayCycle = DayCycles.Night;
+        globalLight.color = night;
+        StartCoroutine(ChangeDayCycle());
+    }
+
+    IEnumerator ChangeDayCycle()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(5f);
+
+            switch (dayCycle)
+            {
+                case DayCycles.Night:
+                    StartCoroutine(ChangeColorOverTime(globalLight, night, day, colorChangeDuration));
+                    dayCycle = DayCycles.Day;
+                    break;
+
+                case DayCycles.Day:
+                    StartCoroutine(ChangeColorOverTime(globalLight, day, dusk, colorChangeDuration));
+                    dayCycle = DayCycles.Dusk;
+                    break;
+
+                case DayCycles.Dusk:
+                    StartCoroutine(ChangeColorOverTime(globalLight, dusk, night, colorChangeDuration));
+                    dayCycle = DayCycles.Night;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+
+    IEnumerator ChangeColorOverTime(UnityEngine.Rendering.Universal.Light2D light, Color startColor, Color endColor, float duration)
+    {
+        float timeElapsed = 0f;
+
+        while (timeElapsed < duration)
+        {
+            timeElapsed += Time.deltaTime;
+            light.color = Color.Lerp(startColor, endColor, timeElapsed / duration);
+            yield return null;
+        }
+
+        light.color = endColor;
+    }
+
+    void ControlLightMaps(bool status)
+    {
+        if (mapLights.Length > 0)
+            foreach (UnityEngine.Rendering.Universal.Light2D _light in mapLights)
+                _light.gameObject.SetActive(status);
+    }
+}
---
+++ client/Werewolf/Assets/Scripts/DayNightSystemAnimator.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightSystemAnimator : MonoBehaviour
+{
+    private Animator animator;
+    private string currentState = "Night_idle";
+    private bool isNight = true;
+    private bool isDay = false;
+    private bool isDusk = false;
+
+    public float waitTime = 5f; // thời gian chờ giữa các chuyển đổi state
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        SetState("Night_idle"); // set initial state to Night_idle
+        StartCoroutine(TransitionState()); // start coroutine to automatically transition state
+    }
+
+    private void SetState(string state)
+    {
+        switch (state)
+        {
+            case "Night_idle":
+                isNight = true;
+                isDay = false;
+                isDusk = false;
+                break;
+            case "Night_Day_transition":
+                isNight = false;
+                isDay = true;
+                isDusk = false;
+                break;
+            case "Day_Dusk_transition":
+                isNight = false;
+                isDay = false;
+                isDusk = true;
+                break;
+            case "Dusk_Night_transition":
+                isNight = true;
+                isDay = false;
+                isDusk = false;
+                break;
+            default:
+                Debug.LogWarning($"Invalid state: {state}");
+                break;
+        }
+
+        animator.SetBool("isDay", isDay);
+        animator.SetBool("isDusk", isDusk);
+        animator.SetBool("isNight", isNight);
+
+        currentState = state;
+    }
+
+    private IEnumerator TransitionState()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime); // wait for 20 seconds
+            if (currentState == "Night_idle")
+            {
+                SetState("Night_Day_transition");
+            }
+            else if (currentState == "Night_Day_transition")
+            {
+                SetState("Day_Dusk_transition");
+            }
+            else if (currentState == "Day_Dusk_transition")
+            {
+                SetState("Dusk_Night_transition");
+            }
+            else if (currentState == "Dusk_Night_transition")
+            {
+                SetState("Night_Day_transition");
+            }
+        }
+    }
+}
