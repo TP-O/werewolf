0508c938c8eea1f539eb8895fc20839d27464e04
Phong Le <69zemo@gmail.com>
2023-03-05 01:49:07 +0700

    feat: player movement and nigga

--- client/Werewolf/Assets/Scripts/PlayerMovement.cs
+++ client/Werewolf/Assets/Scripts/PlayerMovement.cs
@@ -8,12 +8,14 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private int speed = 5;
     private Vector2 movement;
     private Rigidbody2D rb;
+    private Animator animator;
 
     public VectorValue startingPosition;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void Start()
@@ -24,6 +26,18 @@ public class PlayerMovement : MonoBehaviour
     private void OnMovement(InputValue value)
     {
         movement = value.Get<Vector2>();
+        if (movement.x != 0 || movement.y != 0)
+        {
+            animator.SetFloat("X", movement.x);
+            animator.SetFloat("Y", movement.y);
+
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+            
     }
 
     private void FixedUpdate()
