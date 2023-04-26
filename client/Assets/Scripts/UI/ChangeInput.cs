842be0784a0087ae4f97dc0371b3ce8aa40f5022
Phong Le <69zemo@gmail.com>
2023-04-26 15:04:57 +0700

    feat: game ui

---
+++ client/Assets/Scripts/UI/ChangeInput.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeInput : MonoBehaviour
+{
+    EventSystem system;
+    public Selectable firstInput;
+    public Button submitButton;
+
+    void Start()
+    {
+        system = EventSystem.current;
+        firstInput.Select();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab) && Input.GetKey(KeyCode.LeftShift))
+        {
+            Selectable previous = system.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnUp();
+            if(previous != null)
+            {
+                previous.Select();
+            }    
+        }
+        else if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            Selectable next = system.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnDown();
+            if (next != null)
+            {
+                next.Select();
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            submitButton.onClick.Invoke();
+            Debug.Log("Login successfully!");
+        }
+    }
+}
